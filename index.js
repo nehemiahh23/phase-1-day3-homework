a605b7fc9943f386a8c6e589d1cddd389f60c3b9
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-02-24 09:49:47 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -10,34 +10,47 @@
 //TODO: append it to the created div
 //////////////////////////////////////////////////////////////
 
+const catCont = document.getElementById("cat-container")
+const dogCont = document.getElementById("dog-container")
 
+fetch("http://localhost:3000/cats")
+.then(response => response.json())
+.then(cats => {
+    cats.forEach(cats => {
+        const cat = document.createElement("div")
+        const nameHead = document.createElement("h3")
+        const imgTag = document.createElement("img")
+        const breedP = document.createElement("p")
+        
+        catCont.append(cat)
+        cat.append(nameHead, imgTag, breedP)
 
-
-
-
-
-
-
-
-
-
-
+        nameHead.innerText = cats.name
+        imgTag.src = cats.image
+        breedP.innerText = cats.breed
+    })
+})
 
 //////////////////////////////////////////////////////////////
 //TODO: Fetch again and repeat to add dogs to the "dog-container"
 //////////////////////////////////////////////////////////////
 
+fetch("http://localhost:3000/dogs")
+.then(response => response.json())
+.then(dogs => {
+    dogs.forEach(dogs => {
+        const dog = document.createElement("div")
+        const nameHead = document.createElement("h3")
+        const imgTag = document.createElement("img")
+        const breedP = document.createElement("p")
+        
+        dogCont.append(dog)
+        dog.append(nameHead, imgTag, breedP)
 
-
-
-
-
-
-
-
-
-
-
-
+        nameHead.innerText = dogs.name
+        imgTag.src = dogs.image
+        breedP.innerText = dogs.breed
+    })
+})
 
 //////////////////////////////////////////////////////////////
